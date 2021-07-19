634a065dd33a0d55194349e54213a64db22bc67e
florianCAL <florian.calatayud.dev@gmail.com>
2021-07-19 11:42:40 +0200

    Essai

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet1
+{
+    class Personne
+    {
+        string m_name;
+        string m_usname;
+        int m_birtday;
+        public void Afficher (string name, string usname, int birtday)
+        {
+            this.m_name = name;
+            this.m_usname = usname;
+            this.m_birtday = birtday;
+
+            Console.WriteLine(name + usname + birtday);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Projet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personne1 = new Personne();
+            var personne2 = new Personne();
+
+            personne1.Afficher("thomas", "Villier", 1990);
+            personne2.Afficher("Nathalie", "Radhouane", 1997);
+
+
+        }
+    }
+}
